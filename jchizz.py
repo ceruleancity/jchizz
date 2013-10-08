3a68f3f9ee22c03107b072943658e5129ddd33ec
mimio <mimio@mimios-iMac.local>
2013-10-08 13:07:36 -0400

    Text field is now read-only

--- jchizz.py
+++ jchizz.py
@@ -54,6 +54,7 @@ class Application(Frame):
 	def create_widgets(self):
 		self.messaging_field = Text(self, width = 110, height = 20, wrap = WORD)
 		self.messaging_field.grid(row = 0, column = 0, columnspan = 2, sticky = W)
+		self.messaging_field.config(state=DISABLED)
 
 		self.entry_field = Entry(self, width = 92)
 		self.entry_field.grid(row = 1, column = 0, sticky = W)
@@ -63,10 +64,12 @@ class Application(Frame):
 		self.label.grid(row = 2, column = 0, sticky = W)
 
 	def add(self, data):
+		self.messaging_field.config(state=NORMAL)
 		self.messaging_field.insert(END, data )
 
 		# Scroll to the end of the messaging field
 		self.messaging_field.yview(END)
+		self.messaging_field.config(state=DISABLED)
 
 	def socket(self):
 		def loop0():
