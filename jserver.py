cf43c3d07b7aa6439f3951b0315afef2a342ddb8
Anthony Gaetani <anthony.gaetani@mimio.com>
2013-10-10 12:09:14 -0400

    Added more exception handling to server

--- jserver.py
+++ jserver.py
@@ -10,13 +10,18 @@ class MainServerSocket(asyncore.dispatcher):
 		self.bind(('',port))
 		self.listen(5)
 	def handle_accept(self):
-		newSocket, address = self.accept()
-		clients[address] = newSocket
-		print ("Connected from ", address)
-		clients_list = clients.values()
-		for i in clients_list:
-			i.send(bytes('Connected from ' + str(address) + '\n', 'UTF-8'))
-		SecondaryServerSocket(newSocket)
+		try:
+			newSocket, address = self.accept()
+			clients[address] = newSocket
+			print ("Connected from ", address)
+			clients_list = clients.values()
+			for i in clients_list:
+				i.send(bytes('Connected from ' + str(address) + '\n', 'UTF-8'))
+			SecondaryServerSocket(newSocket)
+		except socket.error:
+			print("Socket error connecting client")
+		except Exception:
+			print("Unknown error connecting client")
 
 class SecondaryServerSocket(asyncore.dispatcher_with_send):
 	def handle_read(self):
@@ -29,15 +34,15 @@ class SecondaryServerSocket(asyncore.dispatcher_with_send):
 	def handle_close(self):
 		try:
 			peername = self.getpeername()
-			del clients[peername]
 			print ("Disconnected from ", peername)
 			clients_list = clients.values()
 			for i in clients_list:
 				i.send(bytes('Disconnected from ' + str(peername) + '\n', 'UTF-8'))
+			del clients[peername]
 		except socket.error:
-			print("Error disconnecting client...")
+			print("Socket error disconnecting client...")
 		except Exception:
-			print("Error unknown")
+			print("Unknown error disconnecting client")
 
 MainServerSocket(21567)
 asyncore.loop( )
